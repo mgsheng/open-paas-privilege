faa4fab323912d8b03c88e992649582f6c135af0
devlili <devlili@localhost>
2016-12-14 09:21:13 +0000

    修改json显示格式

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/dao/ResourceUrl.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/dao/ResourceUrl.java
@@ -1,21 +1,44 @@
 package cn.com.open.opensass.privilege.dao;
 
+import cn.com.open.opensass.privilege.model.PrivilegeResource;
+
+import java.util.List;
+
 /**
  * Created by jh on 2016/12/13.
  */
 public class ResourceUrl {
     /*key*/
-    private String key;
+   /* private String key;*/
     /*url地址*/
     private String url;
+    /*private String urlFun;*/
 
-    public String getKey() {
+    /*private List<PrivilegeResource> privilegeResources;
+
+    public List<PrivilegeResource> getPrivilegeResources() {
+        return privilegeResources;
+    }*/
+
+    /*public void setPrivilegeResources(List<PrivilegeResource> privilegeResources) {
+        this.privilegeResources = privilegeResources;
+    }*/
+
+   /* public String getKey() {
         return key;
     }
 
     public void setKey(String key) {
         this.key = key;
+    }*/
+
+   /* public String getUrlFun() {
+        return urlFun;
     }
+
+    public void setUrlFun(String urlFun) {
+        this.urlFun = urlFun;
+    }*/
 
     public String getUrl() {
         return url;
--- privilege-service/src/main/java/cn/com/open/opensass/privilege/dao/cache/RedisDao.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/dao/cache/RedisDao.java
@@ -7,13 +7,16 @@ import com.dyuproject.protostuff.LinkedBuffer;
 import com.dyuproject.protostuff.ProtostuffIOUtil;
 import com.dyuproject.protostuff.Schema;
 import com.dyuproject.protostuff.runtime.RuntimeSchema;
+import net.sf.json.JSONObject;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import redis.clients.jedis.Jedis;
 import redis.clients.jedis.JedisPool;
 import redis.clients.jedis.JedisPoolConfig;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by jh on 2016/12/13.
@@ -64,12 +67,70 @@ public class RedisDao {
     }
 
     /**
+     * 修改jedis
+     * @param resourceUrlData
+     * @param appid
+     * @param uid
+     * @return
+     */
+    public String updateUrlRedis(ResourceUrlData resourceUrlData, String appid, String uid)
+    {
+        try{
+            Jedis jedis = jedisPool.getResource();
+            try{
+                String key = RedisConstant.USERPRIVILEGES_CACHE+appid+ RedisConstant.SIGN+uid;
+                jedis.del(key);
+                byte[] bytes = ProtostuffIOUtil.toByteArray(resourceUrlData,schema,
+                        LinkedBuffer.allocate(LinkedBuffer.DEFAULT_BUFFER_SIZE));
+                /*超时缓存*/
+                int timeout = jedis.getClient().getTimeout();
+                String result = jedis.setex(key.getBytes(),timeout,bytes);
+                return result;
+            }
+            finally {
+                jedis.quit();
+            }
+        }
+        catch (Exception e)
+        {
+            log.error(e.getMessage(),e);
+        }
+        return null;
+    }
+
+    /**
+     * 删除jedis
+     * @param appid
+     * @param uid
+     * @return
+     */
+    public boolean deleteUrlRedis(String appid, String uid)
+    {
+        try{
+            Jedis jedis = jedisPool.getResource();
+            try{
+                String key = RedisConstant.USERPRIVILEGES_CACHE+appid+ RedisConstant.SIGN+uid;
+                jedis.del(key);
+                return true;
+            }
+            finally {
+                jedis.quit();
+            }
+        }
+        catch (Exception e)
+        {
+            log.error(e.getMessage(),e);
+        }
+        return false;
+    }
+
+    /**
      * 从jedis读取
      * @param appid
      * @param uid
      * @return
      */
-    public ResourceUrlData getUrlRedis(String appid, String uid)
+    public String getUrlRedis(String appid, String uid)
     {
         try
         {
@@ -82,7 +143,11 @@ public class RedisDao {
                 {
                     ResourceUrlData resourceUrlData = (ResourceUrlData) schema.newMessage();
                     ProtostuffIOUtil.mergeFrom(bytes,resourceUrlData,schema);
-                    return  resourceUrlData;
+
+                    Map b=new HashMap();
+                    b.put("menuList", resourceUrlData.getResourceUrls());
+                    String menuList= JSONObject.fromObject(b).toString();
+                    return  menuList;
                 }
             }
             finally {
@@ -107,10 +172,13 @@ public class RedisDao {
     public boolean isExistUserKey(String appid, String uid)
     {
         boolean exist = false;
-        ResourceUrlData resourceUrlData = getUrlRedis(appid,uid);
+        ResourceUrlData resourceUrlData =  null
+                /*getUrlRedis(appid,uid)*/;
         if(null != resourceUrlData)
         {
-            final String key = RedisConstant.USERPRIVILEGES_CACHE+appid+ RedisConstant.SIGN+uid;
+            exist = true;
+
+            /*final String key = RedisConstant.USERPRIVILEGES_CACHE+appid+ RedisConstant.SIGN+uid;
             List<ResourceUrl> resourceUrlList = resourceUrlData.getResourceUrls();
 
             for (ResourceUrl resourceUrl : resourceUrlList)
@@ -119,7 +187,7 @@ public class RedisDao {
                 {
                     exist = true;
                 }
-            }
+            }*/
         }
         return exist;
     }
@@ -134,20 +202,20 @@ public class RedisDao {
     public boolean isExistUserValue(String appid, String uid,String url)
     {
         boolean exist = false;
-        ResourceUrlData resourceUrlData = getUrlRedis(appid,uid);
+        ResourceUrlData resourceUrlData =null;
         if(null != resourceUrlData)
         {
-            final String key = RedisConstant.USERPRIVILEGES_CACHE+appid+ RedisConstant.SIGN+uid;
+            exist = true;
+            /*final String key = RedisConstant.USERPRIVILEGES_CACHE+appid+ RedisConstant.SIGN+uid;
             List<ResourceUrl> resourceUrlList = resourceUrlData.getResourceUrls();
-            /*List<ResourceUrl> pilotList = resourceUrlList.stream().filter(r->r.getKey().equals(key) && r.getUrl().indexOf(url)>0).collect(Collectors.toList());*/
+            *//*List<ResourceUrl> pilotList = resourceUrlList.stream().filter(r->r.getKey().equals(key) && r.getUrl().indexOf(url)>0).collect(Collectors.toList());*//*
 
             for (ResourceUrl resourceUrl : resourceUrlList)
             {
                 if(resourceUrl.getKey().equals(key.trim()) && (resourceUrl.getUrl().indexOf(url)>-1 || url.indexOf(resourceUrl.getUrl())>-1))
                 {
-                    exist = true;
                 }
-            }
+            }*/
         }
         return exist;
     }
