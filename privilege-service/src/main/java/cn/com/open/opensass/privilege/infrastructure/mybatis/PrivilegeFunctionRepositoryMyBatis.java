f8e9c6521e9022c2f1c46b647e4908123931fc45
devlili <devlili@localhost>
2016-12-15 01:16:07 +0000

    资源相关操作基本搭建

---
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/infrastructure/mybatis/PrivilegeFunctionRepositoryMyBatis.java
@@ -0,0 +1,9 @@
+package cn.com.open.opensass.privilege.infrastructure.mybatis;
+
+import cn.com.open.opensass.privilege.infrastructure.repository.PrivilegeFunctionRepository;
+
+/**
+ * Created by jh on 2016/12/15.
+ */
+public interface PrivilegeFunctionRepositoryMyBatis extends PrivilegeFunctionRepository {
+}
---
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/infrastructure/repository/PrivilegeFunctionRepository.java
@@ -0,0 +1,7 @@
+package cn.com.open.opensass.privilege.infrastructure.repository;
+
+/**
+ * Created by jh on 2016/12/15.
+ */
+public interface PrivilegeFunctionRepository {
+}
