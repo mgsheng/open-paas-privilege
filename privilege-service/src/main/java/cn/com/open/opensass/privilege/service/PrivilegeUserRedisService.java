b5a21cf5f55dd279bfdfc4c1dd10cb30647d6d65
devtengf <devtengf@localhost>
2016-12-22 07:26:19 +0000

    添加了 更新 删除缓存方法

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/service/PrivilegeUserRedisService.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/service/PrivilegeUserRedisService.java
@@ -7,4 +7,6 @@ import cn.com.open.opensass.privilege.vo.PrivilegeAjaxMessage;
 
 public interface PrivilegeUserRedisService {
 	PrivilegeAjaxMessage getRedisUserRole(String appId,String appUserId);
+	PrivilegeAjaxMessage delUserRoleRedis(String appId, String appUserId);
+	PrivilegeAjaxMessage updateUserRoleRedis(String appId, String appUserId);
 }
