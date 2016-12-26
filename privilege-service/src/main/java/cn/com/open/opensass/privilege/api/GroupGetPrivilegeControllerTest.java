207a7f04f38ff59956392cd70bbf524fb24352d7
devtengf <devtengf@localhost>
2016-12-26 04:37:48 +0000

    添加 删除 更新 组织权限的接口调用方法

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/api/GroupGetPrivilegeControllerTest.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/api/GroupGetPrivilegeControllerTest.java
@@ -16,9 +16,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import cn.com.open.opensass.privilege.redis.impl.RedisClientTemplate;
 import cn.com.open.opensass.privilege.service.PrivilegeGroupService;
 import cn.com.open.opensass.privilege.tools.BaseControllerUtil;
+import cn.com.open.opensass.privilege.tools.WebUtils;
+import cn.com.open.opensass.privilege.vo.PrivilegeAjaxMessage;
 
 /**
- * 测试组织机构权限查询接口
+ * redis groupTest 接口 
+ * 获取数据：getGroupPrivilegeRedis?appId=&groupId=
+ * 更新数据：updateGroupPrivilegeRedis?appId=&groupId=
+ * 删除数据：deleteGroupPrivilegeRedis?appId=&groupId=
  */
 @Controller
 @RequestMapping("/groupTest/")
@@ -26,40 +31,78 @@ public class GroupGetPrivilegeControllerTest extends BaseControllerUtil {
 	private static final Logger log = LoggerFactory.getLogger(GroupGetPrivilegeControllerTest.class);
 	@Autowired
 	private PrivilegeGroupService privilegeGroupService;
-	@Autowired
-	private RedisClientTemplate redisClientTemplate;
 
 	/**
-	 * 组织机构权限查询
+	 * 组织机构权限缓存接口
 	 * 
 	 * @return Json
 	 */
 
-	@RequestMapping("getGroupPrivilegeTest")
+	@RequestMapping("getGroupPrivilegeRedis")
 	public void getGroupPrivilege(HttpServletRequest request, HttpServletResponse response) {
 		Map<String, Object> map = new HashMap<String, Object>();
-		String groupId = request.getParameter("groupId");
-		String appId = request.getParameter("appId");
+		String groupId = request.getParameter("groupId").trim();
+		String appId = request.getParameter("appId").trim();
 		log.info("====================query start======================");
 		if (!paraMandatoryCheck(Arrays.asList(groupId, appId))) {
 			paraMandaChkAndReturn(10000, response, "必传参数中有空值");
 			return;
 		}
-		try {
-			// 查询组织机构缓存
-			Map<String, Object> groupPrivilege = privilegeGroupService.findGroupPrivilege(groupId, appId);
-			if (groupPrivilege != null && groupPrivilege.size() > 0) {
-				writeSuccessJson(response, groupPrivilege);
-				return;
-			} else {
-				map.put("status", "0");
-				map.put("error_code", "10001");
-				writeErrorJson(response, map);
-				return;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+		
+		PrivilegeAjaxMessage message=privilegeGroupService.findGroupPrivilege(groupId, appId);
+		
+		if (message.getCode().equals("1")) {
+			System.err.println(message.getMessage());
+			WebUtils.writeJson(response, message.getMessage());
+		} else {
+			map.put("status", message.getCode());
+			map.put("error_code", message.getMessage());/* 数据不存在 */
+			writeErrorJson(response, map);
 		}
-
+		return;
 	}
+	@RequestMapping("deleteGroupPrivilegeRedis")
+	public void delGroupPrivilege(HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String groupId = request.getParameter("groupId").trim();
+		String appId = request.getParameter("appId").trim();
+		log.info("====================query start======================");
+		if (!paraMandatoryCheck(Arrays.asList(groupId, appId))) {
+			paraMandaChkAndReturn(10000, response, "必传参数中有空值");
+			return;
+		}
+		PrivilegeAjaxMessage ajaxMessage=privilegeGroupService.delGroupPrivilegeCache(groupId, appId);
+		if (ajaxMessage.getCode().equals("1")) {
+			System.err.println(ajaxMessage.getMessage());
+			WebUtils.writeJson(response, ajaxMessage.getMessage());
+		} else {
+			map.put("status", ajaxMessage.getCode());
+			map.put("error_code", ajaxMessage.getMessage());/* 数据不存在 */
+			writeErrorJson(response, map);
+		}
+		return;
+		
+	}
+	@RequestMapping("updateGroupPrivilegeRedis")
+	public void updateGroupPrivilege(HttpServletRequest request, HttpServletResponse response) {
+		Map<String, Object> map = new HashMap<String, Object>();
+		String groupId = request.getParameter("groupId").trim();
+		String appId = request.getParameter("appId").trim();
+		log.info("====================query start======================");
+		if (!paraMandatoryCheck(Arrays.asList(groupId, appId))) {
+			paraMandaChkAndReturn(10000, response, "必传参数中有空值");
+			return;
+		}
+		PrivilegeAjaxMessage ajaxMessage=privilegeGroupService.updateGroupPrivilegeCache(groupId, appId);
+		if (ajaxMessage.getCode().equals("1")) {
+			System.err.println(ajaxMessage.getMessage());
+			WebUtils.writeJson(response, ajaxMessage.getMessage());
+		} else {
+			map.put("status", ajaxMessage.getCode());
+			map.put("error_code", ajaxMessage.getMessage());/* 数据不存在 */
+			writeErrorJson(response, map);
+		}
+		return;
+	}
+	
 }
