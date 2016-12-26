47075eb06632f94d25febae0ccb73962f28a0051
devtengf <devtengf@localhost>
2016-12-26 04:36:58 +0000

    修改 查询 ，更新，删除 组织权限缓存方法 返回类型为PrivilegeAjaxMessage

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/service/impl/PrivilegeGroupServiceImpl.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/service/impl/PrivilegeGroupServiceImpl.java
@@ -6,9 +6,12 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import cn.com.open.opensass.privilege.dao.cache.RedisDao;
 import cn.com.open.opensass.privilege.infrastructure.repository.PrivilegeGroupRepository;
 import cn.com.open.opensass.privilege.infrastructure.repository.PrivilegeMenuRepository;
 import cn.com.open.opensass.privilege.infrastructure.repository.PrivilegeResourceRepository;
@@ -19,6 +22,7 @@ import cn.com.open.opensass.privilege.redis.impl.RedisClientTemplate;
 import cn.com.open.opensass.privilege.redis.impl.RedisConstant;
 import cn.com.open.opensass.privilege.service.PrivilegeGroupService;
 import cn.com.open.opensass.privilege.service.PrivilegeMenuService;
+import cn.com.open.opensass.privilege.vo.PrivilegeAjaxMessage;
 import cn.com.open.opensass.privilege.vo.PrivilegeMenuVo;
 import net.sf.json.JSONObject;
 
@@ -38,8 +42,13 @@ public class PrivilegeGroupServiceImpl implements PrivilegeGroupService {
 	private RedisClientTemplate redisClientTemplate;
 	@Autowired
 	private PrivilegeMenuService privilegeMenuService;
+	@Autowired
+	private RedisDao redisDao;
 	// 缓存前缀
 	private String groupCachePrefix = RedisConstant.PRIVILEGE_GROUPCACHE;
+	// 缓存间隔符
+	public static final String SIGN = RedisConstant.SIGN;
+	private static final Logger log = LoggerFactory.getLogger(PrivilegeGroupServiceImpl.class);
 
 	@Override
 	public Boolean savePrivilegeGroup(PrivilegeGroup privilegeGroup) {
@@ -70,90 +79,72 @@ public class PrivilegeGroupServiceImpl implements PrivilegeGroupService {
 
 	// 查询组织机构
 	@Override
-	public Map<String, Object> findGroupPrivilege(String groupId, String appId) {
-		Map<String, Object> map = new HashMap<String, Object>();
-		if (groupId == null ||("".equals(groupId)) || appId == null || ("".equals(appId))) {
-			map.put("status", "0");
-			map.put("error_code", "10001");
-			return map;
+	public PrivilegeAjaxMessage findGroupPrivilege(String groupId, String appId) {
+		PrivilegeAjaxMessage ajaxMessage = new PrivilegeAjaxMessage();
+		PrivilegeGroup group = privilegeGroupRepository.findByGroupId(groupId, appId);
+		if (null == group) {
+			ajaxMessage.setCode("0");
+			ajaxMessage.setMessage("Group Is Null");
+			return ajaxMessage;
 		}
-		String PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID = groupCachePrefix + appId + "_" + groupId;
-		// 清空缓存
-		//redisClientTemplate.del(PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID);
-		try {
-			// 查询组织机构缓存
-			String str = redisClientTemplate.getString(PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID);
-			if (str != null) {
-				JSONObject jsonObject = JSONObject.fromObject(str);
-				Map map2 = (Map) jsonObject;
-				if (map2 != null && map2.size() > 0) {// 查询缓存命中
-					return map2;
-				}
-			} else {
-				Map<String, Object> redisMap = new HashMap<String, Object>();
-				// 根据组Id和appId 查询资源
-				List<PrivilegeResource> resourceList = privilegeResourceRepository.findByGroupIdAndAppId(groupId,appId);
-				if (resourceList!=null && resourceList.size()>0) {
-					redisMap.put("resourceList", resourceList);
-				}
-				//根据appid查询菜单
-				List<PrivilegeMenu> menuList = privilegeMenuRepository.findMenuByAppId(appId);
-				
-				Set<PrivilegeMenuVo> privilegeMenuVoSet = new HashSet<PrivilegeMenuVo>();
-				//获取所有父菜单
-				Set<PrivilegeMenuVo> allMenu = privilegeMenuService.getAllMenuByUserId(menuList, privilegeMenuVoSet);
-					redisMap.put("menuList", allMenu);
-				// 向redis中添加组织机构的缓存
-				redisClientTemplate.setString(PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID,
-						JSONObject.fromObject(redisMap).toString());
-				return redisMap;
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+
+		String PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID = groupCachePrefix + appId + SIGN + groupId;
+
+		/* 缓存中是否存在 存在返回 */
+		String jsonString = redisClientTemplate.getString(PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID);
+		if (null != jsonString && jsonString.length() > 0) {
+			log.info("获取到缓存");
+			ajaxMessage.setCode("1");
+			ajaxMessage.setMessage(jsonString);
+			System.err.println("缓存" + jsonString);
+			return ajaxMessage;
 		}
-		
-		return null;
+		log.info("从数据库获取数据");
+		Map<String, Object> redisMap = new HashMap<String, Object>();
+		// 根据组Id和appId 查询资源
+		List<Map<String, Object>> resourceList = privilegeResourceRepository.findByGroupIdAndAppId(groupId, appId);
+		redisMap.put("resourceList", resourceList);
+		// 根据groupId appId查询菜单
+		List<PrivilegeMenu> menuList = privilegeMenuRepository.findMenuByGroupIdAndAppId(groupId, appId);
+
+		Set<PrivilegeMenuVo> privilegeMenuVoSet = new HashSet<PrivilegeMenuVo>();
+		// 获取所有父菜单
+		Set<PrivilegeMenuVo> allMenu = privilegeMenuService.getAllMenuByUserId(menuList, privilegeMenuVoSet);
+		redisMap.put("menuList", allMenu);
+		// 向redis中添加组织机构的缓存
+		redisClientTemplate.setString(PRIVILEGESERVICE_GROUPCACHE_APPID_GROUPID,
+				JSONObject.fromObject(redisMap).toString());
+
+		ajaxMessage.setCode("1");
+		ajaxMessage.setMessage(JSONObject.fromObject(redisMap).toString());
+		return ajaxMessage;
+
 	}
-	
+
 	/**
 	 * 删除组织机构的缓存
 	 */
 	@Override
-	public boolean delGroupPrivilegeCache(String groupId, String appId) {
-		if (groupId == null ||("".equals(groupId)) || appId == null || ("".equals(appId))) {
-			return false;
-		}
-		String privilegeservice_groupcache_appid_groupid = groupCachePrefix + appId+"_" + groupId;
-		redisClientTemplate.del(privilegeservice_groupcache_appid_groupid);
-		return true;
+	public PrivilegeAjaxMessage delGroupPrivilegeCache(String groupId, String appId) {
+		PrivilegeAjaxMessage ajaxMessage = new PrivilegeAjaxMessage();
+		Boolean KeyExist = redisDao.deleteRedisKey(groupCachePrefix, appId, groupId);
+		ajaxMessage.setCode("1");
+		ajaxMessage.setMessage(KeyExist ? "Success" : "Failed");
+		log.info("缓存接口删除：" + KeyExist);
+		return ajaxMessage;
 	}
-	
+
 	/**
 	 * 更新组织机构缓存
 	 */
 	@Override
-	public boolean updateGroupPrivilegeCache(String groupId, String appId) {
-		if (groupId == null ||("".equals(groupId)) || appId == null || ("".equals(appId))) {
-			return false;
+	public PrivilegeAjaxMessage updateGroupPrivilegeCache(String groupId, String appId) {
+		boolean RedisKeyExist = redisDao.existKeyRedis(groupCachePrefix, appId, groupId);
+		if (RedisKeyExist) {
+			delGroupPrivilegeCache(groupId, appId);
 		}
-		String privilegeservice_groupcache_appid_groupid = groupCachePrefix + appId + "_" + groupId;
-		Map<String, Object> redisMap = new HashMap<String, Object>();
-		// 根据组Id和appId 查询资源
-		List<PrivilegeResource> resourceList = privilegeResourceRepository.findByGroupIdAndAppId(groupId,appId);
-		if (resourceList!=null && resourceList.size()>0) {
-			redisMap.put("resourceList", resourceList);
-		}
-		//根据appid查询菜单
-		List<PrivilegeMenu> menuList = privilegeMenuRepository.findMenuByAppId(appId);
-		
-		Set<PrivilegeMenuVo> privilegeMenuVoSet = new HashSet<PrivilegeMenuVo>();
-		//获取所有父菜单
-		Set<PrivilegeMenuVo> allMenu = privilegeMenuService.getAllMenuByUserId(menuList, privilegeMenuVoSet);
-			redisMap.put("menuList", allMenu);
-		// 向redis中添加组织机构的缓存
-		redisClientTemplate.setString(privilegeservice_groupcache_appid_groupid,
-				JSONObject.fromObject(redisMap).toString());
-		return true;
+		log.info("更新redis");
+		return findGroupPrivilege(groupId, appId);
 	}
-	
+
 }
