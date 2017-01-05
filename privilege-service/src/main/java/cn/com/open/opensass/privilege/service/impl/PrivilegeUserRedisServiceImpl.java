db1a7de357b5b99677a6c2d2a8633e76e8f38e43
devtengf <devtengf@localhost>
2017-01-05 07:33:01 +0000

    getRedisUserRole方法中 添加 从用户表获取 resourceList  functionList 功能

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/service/impl/PrivilegeUserRedisServiceImpl.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/service/impl/PrivilegeUserRedisServiceImpl.java
@@ -1,5 +1,7 @@
 package cn.com.open.opensass.privilege.service.impl;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import cn.com.open.opensass.privilege.dao.cache.RedisDao;
+import cn.com.open.opensass.privilege.model.PrivilegeFunction;
 import cn.com.open.opensass.privilege.model.PrivilegeRole;
 import cn.com.open.opensass.privilege.model.PrivilegeUser;
 import cn.com.open.opensass.privilege.redis.impl.RedisClientTemplate;
@@ -20,6 +23,7 @@ import cn.com.open.opensass.privilege.service.PrivilegeRoleService;
 import cn.com.open.opensass.privilege.service.PrivilegeUserRedisService;
 import cn.com.open.opensass.privilege.service.PrivilegeUserService;
 import cn.com.open.opensass.privilege.vo.PrivilegeAjaxMessage;
+import cn.com.open.opensass.privilege.vo.PrivilegeResourceVo;
 import net.sf.json.JSONObject;
 
 @Repository("privilegeUserRedisService")
@@ -51,6 +55,8 @@ public class PrivilegeUserRedisServiceImpl implements PrivilegeUserRedisService 
 			ajaxMessage.setMessage("User Is Null");
 			return ajaxMessage;
 		}
+		String privilegeResourceIds = privilegeUser.getResourceId();
+		String privilegeFunctionIds = privilegeUser.getPrivilegeFunId();
 		Map<String, Object> roleMap = new HashMap<String, Object>();
 
 		// redis key
@@ -68,24 +74,67 @@ public class PrivilegeUserRedisServiceImpl implements PrivilegeUserRedisService 
 		List<Map<String, Object>> roles = privilegeRoleService.getRoleListByUserId(appUserId, appId);
 		roleMap.put("roleList", roles);
 		List<PrivilegeRole> roleList = privilegeRoleService.getRoleListByUserIdAndAppId(appUserId, appId);
-		List resourceList=null;
-		Boolean boo=false;
+		List resourceList = null;
+		Boolean boo = false;
 		for (PrivilegeRole role : roleList) {
 			if (role.getRoleType() == 2) {// 若角色为系统管理员 则把app拥有的所有资源放入缓存
 				resourceList = privilegeResourceService.getResourceListByAppId(appId);
-				boo=true;
+				boo = true;
 			}
 
 		}
 
 		// resourceList
 		if (!boo) {
-			
 			resourceList = privilegeResourceService.getResourceListByUserId(appUserId, appId);
+			if (privilegeResourceIds != null && !("").equals(privilegeResourceIds)) {
+				String[] resourceIds1 = privilegeResourceIds.split(",");// 将当前user
+																		// privilegeResourceIds字段数组转list
+				List<String> resourceIdList = new ArrayList<String>();
+				Collections.addAll(resourceIdList, resourceIds1);
+				PrivilegeResourceVo resource = null;
+				// List<Map<String, Object>> resourceList = new
+				// ArrayList<Map<String, Object>>();
+				for (String resourceId : resourceIdList) {
+					Map<String, Object> map2 = new HashMap<String, Object>();
+					resource = privilegeResourceService.findByResourceId(resourceId);
+					map2.put("appId", resource.getAppId());
+					map2.put("resourceId", resource.getResourceId());
+					map2.put("resourceLevel", resource.getResourceLevel());
+					map2.put("resourceName", resource.getResourceName());
+					map2.put("resourceRule", resource.getResourceRule());
+					map2.put("dislayOrder ", resource.getDisplayOrder());
+					map2.put("menuId", resource.getMenuId());
+					map2.put("baseUrl", resource.getBaseUrl());
+					map2.put("status", resource.getStatus());
+					resourceList.add(map2);
+
+				}
+				// roleMap.put("resourceList", resourceList);
+			}
 		}
 		roleMap.put("resourceList", resourceList);
 		// functionList
 		List<Map<String, Object>> functionList = privilegeFunctionService.getFunctionListByUserId(appUserId, appId);
+		// functionList
+		if (privilegeFunctionIds != null && !("").equals(privilegeFunctionIds)) {
+			String[] functionIds1 = privilegeFunctionIds.split(",");
+			List<String> functionIdList = new ArrayList<String>();
+			Collections.addAll(functionIdList, functionIds1);
+			PrivilegeFunction function = null;
+			// List<Map<String, Object>> functionList = new
+			// ArrayList<Map<String, Object>>();
+			for (String functionId : functionIdList) {
+				Map<String, Object> map2 = new HashMap<String, Object>();
+				function = privilegeFunctionService.findByFunctionId(functionId);
+				map2.put("resourceId", function.getResourceId());
+				map2.put("functionId", function.getId());
+				map2.put("optId", function.getOperationId());
+				map2.put("optUrl", function.getOptUrl());
+				functionList.add(map2);
+			}
+			//roleMap.put("functionList", functionList);
+		}
 		roleMap.put("functionList", functionList);
 
 		redisClientTemplate.setString(userCacheRoleKey, JSONObject.fromObject(roleMap).toString());
