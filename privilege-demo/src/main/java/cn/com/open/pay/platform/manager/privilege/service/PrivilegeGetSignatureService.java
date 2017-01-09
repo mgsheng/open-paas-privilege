cbd3438a3e2501b041e085a2fbc293168cf2581e
devtengf <devtengf@localhost>
2017-01-09 00:56:45 +0000

    对 参数进行加密

---
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/service/PrivilegeGetSignatureService.java
@@ -0,0 +1,13 @@
+package cn.com.open.pay.platform.manager.privilege.service;
+
+import java.util.Map;
+
+
+
+public interface PrivilegeGetSignatureService {
+   
+	
+
+	Map<String, Object> getSignature(String appId);
+    
+}
