c6194d4520e6799eae4e4bb5092f748a9f0dbd37
devtengf <devtengf@localhost>
2017-01-11 07:10:46 +0000

    添加 1，跳转授权功能页面方法 	2，查询当前用户权限功能方法 	3，修改当前用户权限功能方法

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/web/ManagerUserController.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/web/ManagerUserController.java
@@ -80,6 +80,163 @@ public class ManagerUserController extends BaseControllerUtil {
 	}
 
 	/**
+	 * 跳转到用户信息列表的页面
+	 * 
+	 * @return 返回的是 jsp文件名路径及文件名
+	 */
+	@RequestMapping(value = "toFunction")
+	public String toFunction(HttpServletRequest request, HttpServletResponse response, Model model)
+			throws UnsupportedEncodingException {
+		log.info("-------------------------toFunction       start------------------------------------");
+		String id = request.getParameter("id");
+		System.err.println("userid==" + id);
+		String userName = request.getParameter("userName");
+		id = (id == null ? null : new String(id.getBytes("iso-8859-1"), "utf-8"));
+		userName = (userName == null ? null : new String(userName.getBytes("iso-8859-1"), "utf-8"));
+		model.addAttribute("id", id);
+		model.addAttribute("userName", userName);
+		String getAppResUrl = "http://localhost:8080/privilege-service/AppRes/getAppResRedis";
+		Map<String, Object> Signature = privilegeGetSignatureService.getSignature("10029");
+		Signature.put("appId", "10029");
+		String reslut = sendPost(getAppResUrl, Signature);
+		List<Map<String, Object>> resourceList = null;
+		List<Map<String, Object>> functionList = null;
+		if (reslut != null && !("").equals(reslut)) {
+			JSONObject jsonObject = JSONObject.fromObject(reslut);
+			Map JsnMap = (Map) jsonObject;
+			if (!("0").equals(JsnMap.get("status"))) {
+				resourceList = (List<Map<String, Object>>) JsnMap.get("resourceList");
+				functionList = (List<Map<String, Object>>) JsnMap.get("functionList");
+				System.err.println("appRes" + JSONObject.fromObject(JsnMap).toString());
+			} else {
+				System.err.println("该应用没有res");
+			}
+		}
+		List<Map<String, Object>> pareRes = new ArrayList<>();
+
+		if (resourceList != null) {
+			for (Map<String, Object> reslist : resourceList) {
+				List<Map<String, Object>> mList = new ArrayList<>();
+				Map<String, Object> map = new HashMap<>();
+				map.put("resourceId", reslist.get("resourceId"));
+				map.put("resourceName", reslist.get("resourceName"));
+				map.put("status", reslist.get("status"));
+				if (functionList != null) {
+					for (Map<String, Object> fun : functionList) {
+						if (fun.get("resourceId").equals(reslist.get("resourceId"))) {
+							Map<String, Object> fMap = new HashMap<>();
+							fMap.put("functionId", fun.get("functionId"));
+							mList.add(fMap);
+							map.put("menus", mList);
+						}
+					}
+				}
+				pareRes.add(map);
+
+			}
+		}
+
+		Map<String, Object> aMap = new HashMap<>();
+		aMap.put("menus", pareRes);
+		model.addAttribute("menus", JSONObject.fromObject(aMap));
+		System.err.println(JSONObject.fromObject(aMap).toString());
+
+		return "show/authorizeFunction";
+	}
+
+	/**
+	 * 授权用户功能
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping("authorizeFun")
+	public void authorizeFunction(HttpServletRequest request, HttpServletResponse response)
+			throws UnsupportedEncodingException {
+		String id = request.getParameter("id");
+		String function = request.getParameter("function");
+		String resource = request.getParameter("resource");
+		System.err.println(function);
+		System.err.println(resource);
+		// String role = request.getParameter("role");
+		String userName = request.getParameter("userName");
+		Boolean boo = false;
+		JSONObject jsonobj = new JSONObject();
+		Map<String, Object> Signature = privilegeGetSignatureService.getSignature(appId);
+		Signature.put("appId", appId);
+		Signature.put("appUserId", id);
+		String reslut = sendPost(getUserPrivilegeUrl, Signature);
+		List<Map<String, Object>> userRoleList = null;
+		if (reslut != null && !("").equals(reslut)) {
+			JSONObject jsonObject = JSONObject.fromObject(reslut);
+			Map JsnMap = (Map) jsonObject;
+			if (!("0").equals(JsnMap.get("status"))) {
+				boo = true;
+			} else {
+				boo = false;
+				// 如果用户不存在 添加用户
+				if (("10002").equals(String.valueOf(JsnMap.get("error_code")))) {
+					Map<String, Object> map = privilegeGetSignatureService.getSignature(appId);
+					map.put("appId", appId);
+					map.put("appUserId", id);
+					map.put("resourceId", resource);
+					map.put("privilegeFunId", function);
+					map.put("appUserName", userName);
+					reslut = sendPost(addPrivilegeUserUrl, map);
+					if (reslut != null && !("").equals(reslut)) {
+						jsonObject = JSONObject.fromObject(reslut);
+						JsnMap = (Map) jsonObject;
+						if (!("0").equals(JsnMap.get("status"))) {
+							boo = true;
+							jsonobj.put("result", boo);
+							WebUtils.writeJson(response, jsonobj);
+							return;
+						} else {
+							boo = false;
+							jsonobj.put("result", boo);
+							WebUtils.writeJson(response, jsonobj);
+							return;
+						}
+					} else {
+						boo = false;
+					}
+				}
+				System.err.println("该用户没有角色");
+			}
+		} else {
+			boo = false;
+		}
+		// 更新资源与功能
+		if (boo) {
+			Map<String, Object> signature = privilegeGetSignatureService.getSignature(appId);
+			signature.put("appId", appId);
+			signature.put("appUserId", id);
+			signature.put("method", "1");
+			signature.put("resourceId", resource);
+			signature.put("privilegeFunId", function);
+			reslut = sendPost(moditUserPrivilegeUrl, signature);
+			if (reslut != null && !("").equals(reslut)) {
+				JSONObject jsonObject = JSONObject.fromObject(reslut);
+				Map JsnMap = (Map) jsonObject;
+				if (!("0").equals(JsnMap.get("status"))) {
+					System.err.println("修改成功");
+					boo = true;
+				} else {
+					boo = false;
+					System.err.println("修改失败");
+				}
+			} else {
+				boo = false;
+			}
+		}
+		jsonobj.put("result", boo);
+		WebUtils.writeJson(response, jsonobj);
+		return;
+
+	}
+
+	/**
 	 * 授权用户角色
 	 * 
 	 * @param request
@@ -109,7 +266,7 @@ public class ManagerUserController extends BaseControllerUtil {
 				userRoleList = (List<Map<String, Object>>) JsnMap.get("roleList");
 			} else {
 				boo = false;
-				//如果用户不存在  添加用户
+				// 如果用户不存在 添加用户
 				if (("10002").equals(String.valueOf(JsnMap.get("error_code")))) {
 					Map<String, Object> map = privilegeGetSignatureService.getSignature(appId);
 					map.put("appId", appId);
@@ -131,18 +288,18 @@ public class ManagerUserController extends BaseControllerUtil {
 							WebUtils.writeJson(response, jsonobj);
 							return;
 						}
-					}else {
-						boo=false;
+					} else {
+						boo = false;
 					}
 				}
 				System.err.println("该用户没有角色");
 			}
-		}else {
-			boo=false;
+		} else {
+			boo = false;
 		}
 		// 用户当前角色ids
 		StringBuffer oldRoleIds = new StringBuffer();
-		if (userRoleList != null&&userRoleList.size()>0) {
+		if (userRoleList != null && userRoleList.size() > 0) {
 			for (Map<String, Object> map : userRoleList) {
 				oldRoleIds.append(map.get("privilegeRoleId"));
 				oldRoleIds.append(",");
@@ -154,6 +311,7 @@ public class ManagerUserController extends BaseControllerUtil {
 			signature.put("appId", appId);
 			signature.put("appUserId", id);
 			signature.put("method", "1");
+			System.err.println("oldId===" + oldRoleIds.subSequence(0, oldRoleIds.length() - 1).toString());
 			signature.put("privilegeRoleId", oldRoleIds.subSequence(0, oldRoleIds.length() - 1).toString());
 			reslut = sendPost(moditUserPrivilegeUrl, signature);
 			if (reslut != null && !("").equals(reslut)) {
@@ -166,8 +324,8 @@ public class ManagerUserController extends BaseControllerUtil {
 					boo = false;
 					System.err.println("删除失败");
 				}
-			}else {
-				boo=false;
+			} else {
+				boo = false;
 			}
 		}
 
@@ -196,32 +354,51 @@ public class ManagerUserController extends BaseControllerUtil {
 					System.err.println("添加失败");
 					boo = false;
 				}
-			}else {
+			} else {
 				boo = false;
 			}
 		}
 		System.out.println("****************id:" + id + "****************role:" + role);
-		// 添加日志
-		/*
-		 * PrivilegeModule privilegeModule =
-		 * privilegeModuleService.getModuleById(55); PrivilegeModule
-		 * privilegeModule1 =
-		 * privilegeModuleService.getModuleById(privilegeModule.getParentId());
-		 * String towLevels= privilegeModule.getName(); String oneLevels =
-		 * privilegeModule1.getName(); String operator = "管理员"; //操作人 String
-		 * operatorId = "1"; //操作人Id PrivilegeResource privilegeResource =
-		 * privilegeResourceService.findByCode("shouquan"); if(boo = true){
-		 * privilegeLogService.addPrivilegeLog(operator,privilegeResource.
-		 * getName(),oneLevels,towLevels,privilegeResource.getId()+"",operator+
-		 * "给"+""+"用户授权成功",operatorId); }else{
-		 * privilegeLogService.addPrivilegeLog(operator,privilegeResource.
-		 * getName(),oneLevels,towLevels,privilegeResource.getId()+"",operator+
-		 * "给"+""+"用户授权失败",operatorId); }
-		 */
+
 		// result = true表示该用户授权角色成功
-		
+
 		jsonobj.put("result", boo);
 		WebUtils.writeJson(response, jsonobj);
+		return;
+	}
+
+	/**
+	 * 查询指定用户的功能情况
+	 * 
+	 * @param request
+	 * @param response
+	 * @throws UnsupportedEncodingException
+	 */
+	@RequestMapping("function")
+	public void function(HttpServletRequest request, HttpServletResponse response) throws UnsupportedEncodingException {
+		String id = request.getParameter("id");
+		id = (id == null ? null : new String(id.getBytes("iso-8859-1"), "utf-8"));
+		Map<String, Object> Signature = privilegeGetSignatureService.getSignature(appId);
+		Signature.put("appId", appId);
+		Signature.put("appUserId", id);
+		String reslut = sendPost(getUserPrivilegeUrl, Signature);
+		List<Map<String, Object>> userResourceList = null;
+		List<Map<String, Object>> userFunctionList = null;
+		if (reslut != null && !("").equals(reslut)) {
+			JSONObject jsonObject = JSONObject.fromObject(reslut);
+			Map JsnMap = (Map) jsonObject;
+			if (!("0").equals(JsnMap.get("status"))) {
+				userResourceList = (List<Map<String, Object>>) JsnMap.get("resourceList");
+				userFunctionList = (List<Map<String, Object>>) JsnMap.get("functionList");
+			} else {
+				System.err.println("该用户没有功能");
+			}
+		}
+		Map<String, Object> aMap = new HashMap<>();
+		aMap.put("resourceList", userResourceList);
+		aMap.put("functionList", userFunctionList);
+		System.err.println(JSONObject.fromObject(aMap).toString());
+		WebUtils.writeJson(response, JSONObject.fromObject(aMap));
 		return;
 	}
 
