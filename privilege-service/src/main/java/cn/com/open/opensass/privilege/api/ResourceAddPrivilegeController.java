7bfd51a25cc35151a1987968a870b71c8867692b
devtengf <devtengf@localhost>
2017-01-22 13:33:09 +0000

    参数加入编码

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/api/ResourceAddPrivilegeController.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/api/ResourceAddPrivilegeController.java
@@ -50,7 +50,7 @@ public class ResourceAddPrivilegeController extends BaseControllerUtil{
     	String createUser="";
     	try {
     	if(!nullEmptyBlankJudge(request.getParameter("resourceName"))){
-    		resourceName = request.getParameter("resourceName");
+    		resourceName = new String(request.getParameter("resourceName").getBytes("iso-8859-1"),"utf-8");	
     	}
     	if(!nullEmptyBlankJudge(request.getParameter("resourceRule"))){
     		resourceRule = new String(request.getParameter("resourceRule").getBytes("iso-8859-1"),"utf-8");	
