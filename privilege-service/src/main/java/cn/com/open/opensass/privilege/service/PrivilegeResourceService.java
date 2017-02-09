9616c5318d440b26a733f680cc8850aad221c2e7
devtengf <devtengf@localhost>
2017-02-09 08:40:34 +0000

    查找用户资源时 条件加入appId 过滤非该应用资源

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/service/PrivilegeResourceService.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/service/PrivilegeResourceService.java
@@ -16,7 +16,7 @@ import cn.com.open.opensass.privilege.vo.PrivilegeResourceVo;
 public interface PrivilegeResourceService {
 	Boolean savePrivilegeResource(PrivilegeResource privilegeResource);
 	PrivilegeResource findByResourceId(String resourceId,String appId);
-	PrivilegeResourceVo findByResourceId(String resourceId);
+	PrivilegeResourceVo findByResource_Id(String resourceId,String appId);
 	PrivilegeResource findByResourceCode(String resourceId,String appId);
 	List<PrivilegeResource>findResourcePage(String menuId,String appId,int startRow,int pageSize,String resourceLevel);
 	Boolean updatePrivilegeResource(PrivilegeResource privilegeResource);
@@ -30,8 +30,7 @@ public interface PrivilegeResourceService {
 	PrivilegeAjaxMessage getAppResRedis(String appId);
 	PrivilegeAjaxMessage delAppResRedis(String appId);
 	PrivilegeAjaxMessage updateAppResRedis(String appId);
-	List<Map<String, Object>> getResourceListByFunIds(String[] functionIds);
-	List<Map<String, Object>> getAllResource(List<Map<String, Object>> resources);
+	List<Map<String, Object>> getResourceListByFunIds(String[] functionIds,String appId);
 	List<Map<String, Object>> getResourceListByMenuId(String menuId);
 	List<String> findAppResources(String appId);
 	PrivilegeResource getResourceListByFunId(String functionId);
