ce97d931793f4665c36600d0b0298c0cbaed8b58
devtengf <devtengf@localhost>
2017-02-10 00:34:01 +0000

    根据资源id查询功能时条件加入appId排除非该应用的功能

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/infrastructure/repository/PrivilegeFunctionRepository.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/infrastructure/repository/PrivilegeFunctionRepository.java
@@ -1,7 +1,6 @@
 package cn.com.open.opensass.privilege.infrastructure.repository;
 
 import java.util.List;
-import java.util.Map;
 
 import org.apache.ibatis.annotations.Param;
 
@@ -12,16 +11,16 @@ import cn.com.open.opensass.privilege.model.PrivilegeFunction;
  */
 public interface PrivilegeFunctionRepository {
     /*根据ID获取值*/
-    PrivilegeFunction findByFunctionId(@Param("functionId")String functionId);
+    PrivilegeFunction findByFunctionId(@Param("functionId")String functionId,@Param("appId")String appId);
 	void savePrivilegeFunction(PrivilegeFunction privilegeFunction);
 	void updatePrivilegeFunction(PrivilegeFunction privilegeFunction);
 	void deleteByFunctionId(@Param("functionId")String functionId);
 	void deleteByFunctionIds(@Param("functionIds")String[] functionIds);
 	List<PrivilegeFunction> findFunctionPage(@Param("functionId")String functionId,@Param("startRow")String startRow,@Param("pageSize")String pageSize);
-	List<PrivilegeFunction> getFunctionByRId(@Param("resourceId")String resourceId);
-	List<PrivilegeFunction> getFunctionMap(@Param("resourceId")String resourceId);
+	List<PrivilegeFunction> getFunctionByRId(@Param("resourceId")String resourceId,@Param("appId")String appId);
+	List<PrivilegeFunction> getFunctionMap(@Param("resourceId")String resourceId,@Param("appId")String appId);
 	List<PrivilegeFunction> getFunctionListByUserId(@Param("appUserId")String appUserId,@Param("appId")String appId);
 	List<PrivilegeFunction> getFunctionListByAppId(@Param("appId")String appId);
-	List<PrivilegeFunction> getFunctionListByFunctionIds(@Param("functionIds")String[] functionIds);
+	List<PrivilegeFunction> getFunctionListByFunctionIds(@Param("functionIds")String[] functionIds,@Param("appId")String appId);
 	List<String> findAppFunction(String appId);
 }
