809587f990a206468122fd752595b3cec94279e8
devtengf <devtengf@localhost>
2017-02-12 23:58:39 +0000

    添加token缓存的key

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/redis/impl/RedisConstant.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/redis/impl/RedisConstant.java
@@ -1,11 +1,15 @@
 package cn.com.open.pay.platform.manager.redis.impl;
+
 /**
  * redis 存储开开头
+ * 
  * @author admin
  *
  */
 public class RedisConstant {
- public static final String APP_INFO="appInfo_";
- public static final String USER_NAME_CHECK="userNameCheck_";
- public static final String USER_CACHE_INFO="userCacheInfo_";//存储方式：key:userCacheInfo_username value:""
+	public static final String ACCESSTOKEN_CACHE="_userservice_accessToken";
+	public static final String APP_INFO = "appInfo_";
+	public static final String USER_NAME_CHECK = "userNameCheck_";
+	public static final String USER_CACHE_INFO = "userCacheInfo_";// 存储方式：key:userCacheInfo_username
+																	// value:""
 }
