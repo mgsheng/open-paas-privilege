8b8d5e2436e9eff8c6758907bc01ae604803d8d4
devtengf <devtengf@localhost>
2017-02-14 00:31:45 +0000

    添加getRoleListByAppIdAndGroupId

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/infrastructure/repository/PrivilegeRoleRepository.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/infrastructure/repository/PrivilegeRoleRepository.java
@@ -8,7 +8,6 @@ import org.springframework.stereotype.Repository;
 
 import cn.com.open.opensass.privilege.model.PrivilegeRole;
 
-
 /**
  * 
  */
@@ -19,18 +18,23 @@ public interface PrivilegeRoleRepository extends Repository {
 	PrivilegeRole findByRoleId(String pararentRoleId);
 
 	void delPrivilegeRoleById(String privilegeRoleId);
-	
-	List<Map<String, Object>> getRoleListByUserId(@Param("appUserId")String appUserId,@Param("appId")String appId);
-	
+
+	List<Map<String, Object>> getRoleListByUserId(@Param("appUserId") String appUserId, @Param("appId") String appId);
+
 	void updatePrivilegeRole(PrivilegeRole privilegeRole);
 
 	List<String> findRoleByAppId(String appId);
 
-	List<PrivilegeRole> findRoleByPage(@Param("privilegeRoleId") String privilegeRoleId,@Param("appId") String appId,@Param("start") int start,@Param("limit") int limit);
+	List<PrivilegeRole> findRoleByPage(@Param("privilegeRoleId") String privilegeRoleId, @Param("appId") String appId,
+			@Param("start") int start, @Param("limit") int limit);
 
 	List<PrivilegeRole> findRoleNoPage(@Param("privilegeRoleId") String privilegeRoleId, @Param("appId") String appId);
-	
+
 	List<PrivilegeRole> getRoleListByAppId(@Param("appId") String appId);
-	List<PrivilegeRole> getRoleListByUserIdAndAppId(@Param("appUserId")String appUserId,@Param("appId")String appId);
-	
+
+	List<PrivilegeRole> getRoleListByUserIdAndAppId(@Param("appUserId") String appUserId, @Param("appId") String appId);
+
+	List<PrivilegeRole> getRoleListByAppIdAndGroupId(@Param("appId") String appId, @Param("groupId") String groupId);
+
+	int getRoleCountByAppIdAndGroupId(@Param("appId") String appId, @Param("groupId") String groupId);
 }
