95e5849fc72e4fac0901e07da626b557b10658ec
liyun <liyun@localhost>
2017-02-15 07:44:24 +0000

    修改sendGet方法utf-8编码

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/tools/BaseControllerUtil.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/tools/BaseControllerUtil.java
@@ -224,7 +224,7 @@ public class BaseControllerUtil {
 	            }
 	            // 定义 BufferedReader输入流来读取URL的响应
 	            in = new BufferedReader(new InputStreamReader(
-	                    connection.getInputStream()));
+	                    connection.getInputStream(), "utf-8"));
 	            String line;
 	            while ((line = in.readLine()) != null) {
 	               result += line;
