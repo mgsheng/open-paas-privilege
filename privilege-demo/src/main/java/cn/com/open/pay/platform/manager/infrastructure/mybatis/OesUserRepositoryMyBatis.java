7ce559793b688899aec471b2bcffc88b7174153e
devtengf <devtengf@localhost>
2017-02-16 07:31:57 +0000

    添加oes_user实体及myBatis类

---
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/infrastructure/mybatis/OesUserRepositoryMyBatis.java
@@ -0,0 +1,9 @@
+package cn.com.open.pay.platform.manager.infrastructure.mybatis;
+
+import cn.com.open.pay.platform.manager.infrastructure.repository.OesUserRepository;
+
+/**
+ * 
+ */
+public interface OesUserRepositoryMyBatis extends OesUserRepository {
+}
