a10794731f07e59f6af18832f4ba45bb7e5aacf0
devtengf <devtengf@localhost>
2017-02-17 08:13:40 +0000

    添加findAll 查找所有组织机构

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/infrastructure/repository/OesGroupRepository.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/infrastructure/repository/OesGroupRepository.java
@@ -15,11 +15,12 @@ import cn.com.open.pay.platform.manager.privilege.model.OesGroup;
 public interface OesGroupRepository extends Repository {
 
 	List<OesGroup> findAllByPage(@Param("startRow") int startRow, @Param("pageSize") int pageSize);
-
+	
 	int findAllCount();
 
 	OesGroup findByCode(String groupCode);
 
 	void saveGroup(OesGroup g);
-
+	
+	List<OesGroup> findAll();
 }
