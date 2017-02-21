32fbadf2113381845ae9f973435b570d0be7ae40
devtengf <devtengf@localhost>
2017-02-21 01:57:04 +0000

    添加oesLatest实体及myBatis类

---
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/model/OesLatestVisit.java
@@ -0,0 +1,46 @@
+package cn.com.open.pay.platform.manager.privilege.model;
+
+import java.util.Date;
+
+
+public class OesLatestVisit {
+	
+	private Integer id;
+	private String userId;
+	private String menuName;
+	private String menuId;
+	private Date createTime = new Date();
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getMenuName() {
+		return menuName;
+	}
+	public void setMenuName(String menuName) {
+		this.menuName = menuName;
+	}
+	public String getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+	
+	
+}
