dd6a052a83d8a7cc3961b0bb8469e90540ca7b8f
devtengf <devtengf@localhost>
2017-02-21 02:00:16 +0000

    添加最近访问缓存key

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/redis/impl/RedisConstant.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/redis/impl/RedisConstant.java
@@ -12,4 +12,10 @@ public class RedisConstant {
 	public static final String USER_NAME_CHECK = "userNameCheck_";
 	public static final String USER_CACHE_INFO = "userCacheInfo_";// 存储方式：key:userCacheInfo_username
 																	// value:""
+	public static final String USER_LATEST_VISIT="userLastVisit_";
+	public static final String PUBLICSERVICE_CACHE="privilegeService_";
+	 /**
+	  * appResCache_appid
+	  */
+	public static final String APPRES_CACHE=PUBLICSERVICE_CACHE+"appResCache_";
 }
