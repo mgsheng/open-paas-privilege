797ab6aed0e8f081001b53ba76c5314ecfa2c987
devtengf <devtengf@localhost>
2017-02-22 00:48:57 +0000

    添加用户最后访问菜单缓存key

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/redis/impl/RedisConstant.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/redis/impl/RedisConstant.java
@@ -12,10 +12,7 @@ public class RedisConstant {
 	public static final String USER_NAME_CHECK = "userNameCheck_";
 	public static final String USER_CACHE_INFO = "userCacheInfo_";// 存储方式：key:userCacheInfo_username
 																	// value:""
+	//用户最后访问菜单缓存key
 	public static final String USER_LATEST_VISIT="userLastVisit_";
-	public static final String PUBLICSERVICE_CACHE="privilegeService_";
-	 /**
-	  * appResCache_appid
-	  */
-	public static final String APPRES_CACHE=PUBLICSERVICE_CACHE+"appResCache_";
+	
 }
