d897d2fd9aabca9ebec3dfb3505759b0f5ee9f5e
devtengf <devtengf@localhost>
2017-02-27 08:34:50 +0000

    添加getGroupPrivilegeUrl方法， 返回组织机构权限url

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/service/PrivilegeUrlService.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/service/PrivilegeUrlService.java
@@ -8,53 +8,70 @@ import cn.com.open.opensass.privilege.vo.PrivilegeAjaxMessage;
  * URL REDIS 操作类
  */
 public interface PrivilegeUrlService {
-    /**
-     * 获取redis url 数据
-     * @param appId 应用id
-     * @param appUserId 对应应用id的用户id
-     * @return 相应的提示信息
-     */
-    PrivilegeAjaxMessage getRedisUrl(String appId, String appUserId);
-    /**
-     * 获取redis url 数据
-     * @param appId 应用id
-     * @param appUserId 对应应用id的用户id
-     * @return 相应的提示信息
-     */
-    PrivilegeAjaxMessage updateRedisUrl(String appId,String appUserId);
-    /**
-     * 获取redis url 数据
-     * @param appId 应用id
-     * @param appUserId 对应应用id的用户id
-     * @return 相应的提示信息
-     */
-    PrivilegeAjaxMessage deleteRedisUrl(String appId,String appUserId);
+	/**
+	 * 获取redis url 数据
+	 * 
+	 * @param appId应用id
+	 * @param appUserId 对应应用id的用户id
+	 * @return 相应的提示信息
+	 */
+	PrivilegeAjaxMessage getRedisUrl(String appId, String appUserId);
 
-    /**
-     *
-     * @param appId 应用id
-     * @param appUserId 对应应用id的用户id
-     * @param url 传入的网址
-     * @return 相应的提示信息
-     */
-    PrivilegeAjaxMessage existRedisUrl(String appId,String appUserId,String url);
-    /**
-     * 获取redis url 数据
-     * @param appId 应用id
-     * @param appUserId 对应应用id的用户id
-     * @return 相应的提示信息
-     */
+	/**
+	 * 获取redis url 数据
+	 * 
+	 * @param appId应用id
+	 * @param appUserId对应应用id的用户id
+	 * @return 相应的提示信息
+	 */
+	PrivilegeAjaxMessage updateRedisUrl(String appId, String appUserId);
 
-    PrivilegeAjaxMessage existRedisKey(String appId,String appUserId);
+	/**
+	 * 获取redis url 数据
+	 * 
+	 * @param appId 应用id
+	 * @param appUserId 对应应用id的用户id
+	 * @return 相应的提示信息
+	 */
+	PrivilegeAjaxMessage deleteRedisUrl(String appId, String appUserId);
 
-    /**
-     * 获取相应的输出信息
-     * @param appId 应用id
-     * @param appUserId 对应应用id的用户id
-     * @param privilegeUser 传入类，获取相关信息
-     * @return
-     */
-    PrivilegeUrl getPrivilegeUrl(String appId, String appUserId, PrivilegeUser privilegeUser);
-    PrivilegeUrl getAllPrivilegeUrl(String appId);
+	/**
+	 *
+	 * @param appId应用id
+	 * @param appUserId 对应应用id的用户id
+	 * @param url传入的网址
+	 * @return 相应的提示信息
+	 */
+	PrivilegeAjaxMessage existRedisUrl(String appId, String appUserId, String url);
+
+	/**
+	 * 获取redis url 数据
+	 * 
+	 * @param appId应用id
+	 * @param appUserId 对应应用id的用户id
+	 * @return 相应的提示信息
+	 */
+
+	PrivilegeAjaxMessage existRedisKey(String appId, String appUserId);
+
+	/**
+	 * 获取相应的输出信息
+	 * 
+	 * @param appId应用id
+	 * @param appUserId对应应用id的用户id
+	 * @param privilegeUser传入类，获取相关信息
+	 * @return
+	 */
+	PrivilegeUrl getPrivilegeUrl(String appId, String appUserId, PrivilegeUser privilegeUser);
+	
+	PrivilegeUrl getAllPrivilegeUrl(String appId);
+	/**
+	 * 获取组织机构 url 数据
+	 * 
+	 * @param appId应用id
+	 * @param groupId 对应组织的id
+	 * @return 相应的提示信息
+	 */
+	PrivilegeUrl getGroupPrivilegeUrl(String appId, String groupId);
 
 }
