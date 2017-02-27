8f0a3905fd894b1ffdb42be25906e221ef045a3e
devtengf <devtengf@localhost>
2017-02-27 09:08:55 +0000

    添加oesFrequentlyUsedMenu实体及myBatis类

---
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/model/OesFrequentlyUsedMenu.java
@@ -0,0 +1,39 @@
+package cn.com.open.pay.platform.manager.privilege.model;
+
+import java.util.Date;
+
+
+public class OesFrequentlyUsedMenu {
+	
+	private Integer id;
+	private String userId;
+	private String menuId;
+	private Date createTime = new Date();
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserId() {
+		return userId;
+	}
+	public void setUserId(String userId) {
+		this.userId = userId;
+	}
+	public String getMenuId() {
+		return menuId;
+	}
+	public void setMenuId(String menuId) {
+		this.menuId = menuId;
+	}
+	public Date getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+	
+	
+	
+}
