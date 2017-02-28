4e9fcfccdd801349478b6c6ad072b1e6ac500f5a
devtengf <devtengf@localhost>
2017-02-28 09:15:27 +0000

    添加用户，删除用户

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/infrastructure/repository/OesUserRepository.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/infrastructure/repository/OesUserRepository.java
@@ -9,10 +9,14 @@ import java.util.Map;
 import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 
+import cn.com.open.pay.platform.manager.privilege.model.OesUser;
+
 
 public interface OesUserRepository extends Repository {
 	
 	List<Map<String, Object>> getUserListByPage(@Param("groupId") String groupId,@Param("start") int start,@Param("limit") int limit,@Param("userName") String userName);
 	int getUserCount(@Param("groupId")String groupId,@Param("userName") String userName);
+	void saveUser(OesUser oesUser);
+	void deleteUser(@Param("Id")Integer Id); 
 	
 }
