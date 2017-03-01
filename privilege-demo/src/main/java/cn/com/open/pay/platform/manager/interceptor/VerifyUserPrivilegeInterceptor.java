504dffa370f6ab554d77860520889a93da659c5a
devtengf <devtengf@localhost>
2017-03-01 03:18:16 +0000

    删除无用的引用

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/interceptor/VerifyUserPrivilegeInterceptor.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/interceptor/VerifyUserPrivilegeInterceptor.java
@@ -8,7 +8,6 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
