bf23d46a8a05cda64df6f695cdd3f1a0a1c553af
devtengf <devtengf@localhost>
2017-03-09 01:19:23 +0000

    重写hashCode,equals 去重

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/vo/PrivilegeResourceVo.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/vo/PrivilegeResourceVo.java
@@ -10,59 +10,93 @@ public class PrivilegeResourceVo {
 	private String menuId;
 	private int displayOrder;
 	private String baseUrl;
-	
+
 	public String getResourceId() {
 		return resourceId;
 	}
+
 	public void setResourceId(String resourceId) {
 		this.resourceId = resourceId;
 	}
+
 	public String getResourceName() {
 		return resourceName;
 	}
+
 	public void setResourceName(String resourceName) {
 		this.resourceName = resourceName;
 	}
+
 	public String getResourceRule() {
 		return resourceRule;
 	}
+
 	public void setResourceRule(String resourceRule) {
 		this.resourceRule = resourceRule;
 	}
+
 	public String getAppId() {
 		return appId;
 	}
+
 	public void setAppId(String appId) {
 		this.appId = appId;
 	}
+
 	public int getStatus() {
 		return status;
 	}
+
 	public void setStatus(int status) {
 		this.status = status;
 	}
+
 	public String getResourceLevel() {
 		return resourceLevel;
 	}
+
 	public void setResourceLevel(String resourceLevel) {
 		this.resourceLevel = resourceLevel;
 	}
+
 	public String getMenuId() {
 		return menuId;
 	}
+
 	public void setMenuId(String menuId) {
 		this.menuId = menuId;
 	}
+
 	public int getDisplayOrder() {
 		return displayOrder;
 	}
+
 	public void setDisplayOrder(int displayOrder) {
 		this.displayOrder = displayOrder;
 	}
+
 	public String getBaseUrl() {
 		return baseUrl;
 	}
+
 	public void setBaseUrl(String baseUrl) {
 		this.baseUrl = baseUrl;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (!(obj instanceof PrivilegeResourceVo)) {
+			return false;
+		}
+		PrivilegeResourceVo privilegeResourceVo = (PrivilegeResourceVo) obj;
+		return resourceId.equals(privilegeResourceVo.getResourceId());
+	}
+
+	@Override
+	public int hashCode() {
+		return resourceId.hashCode();
 	}
 }
