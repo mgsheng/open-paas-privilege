334b59cc772d6437996a36713e7cdca9c7da1df9
devtengf <devtengf@localhost>
2017-03-16 07:05:49 +0000

    添加getToken

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/service/PrivilegeGetSignatureService.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/service/PrivilegeGetSignatureService.java
@@ -2,12 +2,11 @@ package cn.com.open.pay.platform.manager.privilege.service;
 
 import java.util.Map;
 
-
-
 public interface PrivilegeGetSignatureService {
-   
-	
-	Map<String, Object> getOauthSignature(String appId,String client_id,String access_token);
+
+	Map<String, Object> getOauthSignature(String appId, String client_id, String access_token);
+
 	Map<String, Object> getSignature(String appId);
-    
+
+	String getToken();
 }
