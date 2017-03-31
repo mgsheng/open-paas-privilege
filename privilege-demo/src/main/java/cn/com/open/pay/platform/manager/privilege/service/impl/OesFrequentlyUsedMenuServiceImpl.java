347481dedc1a2034695b74a975dbc5bc3748b245
devtengf <devtengf@localhost>
2017-03-31 05:33:56 +0000

    读取用户常用菜单缓存时，用户常用菜单缓存版本号与应用菜单版本号对比，如果不相同，更新用户常用菜单缓存

--- privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/service/impl/OesFrequentlyUsedMenuServiceImpl.java
+++ privilege-demo/src/main/java/cn/com/open/pay/platform/manager/privilege/service/impl/OesFrequentlyUsedMenuServiceImpl.java
@@ -26,6 +26,7 @@ public class OesFrequentlyUsedMenuServiceImpl extends BaseControllerUtil impleme
 	private RedisClientTemplate redisClientTemplate;
 	private static final String USER_FREQUENTLY_MENU = RedisConstant.USER_FREQUENTLY_MENU;
 	private static final String SIGN = RedisConstant.SIGN;
+	private static final String appMenuVersionCache = RedisConstant.APPMENUVERSIONCACHE;
 	@Autowired
 	private OesFrequentlyUsedMenuRepository oesFrequentlyUsedMenuRepository;
 
@@ -56,12 +57,29 @@ public class OesFrequentlyUsedMenuServiceImpl extends BaseControllerUtil impleme
 
 	@Override
 	public List<Map<String, Object>> getUserFrequentlyMenuRedis(String userId, String appId) {
+		//从缓存中获取应用菜单版本
+		Integer appMenuVersion =  (Integer) redisClientTemplate.getObject(appMenuVersionCache+appId);
 		//从缓存中取，如果有，返回
 		String json = redisClientTemplate.getString(USER_FREQUENTLY_MENU + appId + SIGN + userId);
 		List<Map<String, Object>> FrequentlyRes = new ArrayList<Map<String, Object>>();
 		if (json != null && !json.isEmpty()) {
 			JSONObject object = JSONObject.fromObject(json);
-			List<Map<String, Object>> menuList = (List<Map<String, Object>>) object.get("menuList");
+			List<Map<String, Object>> menuList = new ArrayList<Map<String, Object>>();
+			//从缓存中获取应用菜单版本，与用户常用菜单缓存版本号对比，若版本号不相同，更新用户常用菜单缓存
+			if (appMenuVersion != null) {
+				Integer userMenuVersion = (Integer) object.get("version");
+				if (userMenuVersion == null) {
+					menuList = updateUserFrequentlyMenuRedis(userId, appId);
+				} else {
+					if (appMenuVersion.equals(userMenuVersion)) {
+						menuList = (List<Map<String, Object>>) object.get("menuList");
+					} else {
+						menuList = updateUserFrequentlyMenuRedis(userId, appId);
+					}
+				}
+				return menuList;
+			}
+			menuList = (List<Map<String, Object>>) object.get("menuList");
 			return menuList;
 		} else {//缓存中没有，从数据库读取
 			OesFrequentlyUsedMenu oesFrequentlyUsedMenu = oesFrequentlyUsedMenuRepository
@@ -96,6 +114,10 @@ public class OesFrequentlyUsedMenuServiceImpl extends BaseControllerUtil impleme
 					}
 					parameter.clear();
 					parameter.put("menuList", FrequentlyRes);
+					//应用菜单版本不为null ，加入 常用菜单缓存版本号
+					if (appMenuVersion != null) {
+						parameter.put("version", appMenuVersion);
+					}
 					//放入redis
 					redisClientTemplate.setString(USER_FREQUENTLY_MENU+appId+SIGN+userId, JSONObject.fromObject(parameter).toString());
 				}
@@ -105,10 +127,9 @@ public class OesFrequentlyUsedMenuServiceImpl extends BaseControllerUtil impleme
 	}
 
 	@Override
-	public Boolean updateUserFrequentlyMenuRedis(String userId, String appId) {
-		Boolean boo=redisClientTemplate.del(USER_FREQUENTLY_MENU+appId+SIGN+userId);
-		getUserFrequentlyMenuRedis(userId, appId);
-		return boo;
+	public List<Map<String, Object>> updateUserFrequentlyMenuRedis(String userId, String appId) {
+		redisClientTemplate.del(USER_FREQUENTLY_MENU+appId+SIGN+userId);
+		return getUserFrequentlyMenuRedis(userId, appId);
 	}
 
 }
