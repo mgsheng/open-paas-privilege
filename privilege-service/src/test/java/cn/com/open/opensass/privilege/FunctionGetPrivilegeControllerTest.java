505a29f5d831e235f782542bc6e7f381bd7682f5
guxuyang <it_gxy@163.com>
2017-08-29 17:50:59 +0800

    获取用户的功能权限接口单元测试

---
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/FunctionGetPrivilegeControllerTest.java
@@ -0,0 +1,30 @@
+package cn.com.open.opensass.privilege;
+
+import cn.com.open.opensass.privilege.api.FunctionGetPrivilegeController;
+import cn.com.open.opensass.privilege.base.BaseTest;
+import cn.com.open.opensass.privilege.signature.Signature;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.UnsupportedEncodingException;
+
+public class FunctionGetPrivilegeControllerTest extends BaseTest {
+
+    @Autowired
+    private FunctionGetPrivilegeController fixture;
+
+    @Test //  /function/getFunctionIdList")
+    public void modifyFunction() throws UnsupportedEncodingException {
+        String appsecret = "67d73cec5d6b4c8a8a9883748f4066fe";
+        String appId = "8";
+        String appKey = "df6bda1a157d49cea82447c3e925dd6d";
+        MockHttpServletRequest request = Signature.getSignatureRequest(appsecret, appId, appKey);
+        request.addParameter("appUserId", "3094776");
+        request.addParameter("resourceId", "151,185");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        fixture.modifyFunction(request, response);
+        log.info(response.getContentAsString());
+    }
+}
