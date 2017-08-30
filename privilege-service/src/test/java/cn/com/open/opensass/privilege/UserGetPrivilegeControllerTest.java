addfa2a62efcd6c89c0893b871f36db423de03e0
guxuyang <it_gxy@163.com>
2017-08-30 09:54:45 +0800

    获取用户菜单接口（树状菜单）单元测试

---
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/UserGetPrivilegeControllerTest.java
@@ -0,0 +1,35 @@
+package cn.com.open.opensass.privilege;
+
+import cn.com.open.opensass.privilege.api.UserGetPrivilegeController;
+import cn.com.open.opensass.privilege.base.BaseTest;
+import cn.com.open.opensass.privilege.signature.Signature;
+import cn.com.open.opensass.privilege.vo.PrivilegeUserVo;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mock.web.MockHttpServletRequest;
+import org.springframework.mock.web.MockHttpServletResponse;
+
+import java.io.UnsupportedEncodingException;
+
+
+public class UserGetPrivilegeControllerTest extends BaseTest {
+	
+	@Autowired
+	private UserGetPrivilegeController fixture;
+
+	@Test
+	public void getPrivilege() throws UnsupportedEncodingException {
+		String appsecret = "67d73cec5d6b4c8a8a9883748f4066fe";
+		String appId = "8";
+		String appKey = "df6bda1a157d49cea82447c3e925dd6d";
+		String appUserId = "3094776";
+
+		MockHttpServletRequest request = Signature.getSignatureRequest(appsecret, appId, appKey);
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		PrivilegeUserVo modifyPrivilegeUserVo = new PrivilegeUserVo();
+		modifyPrivilegeUserVo.setAppId(appId);
+		modifyPrivilegeUserVo.setAppUserId(appUserId);
+		fixture.getPrivilege(request, response, modifyPrivilegeUserVo);
+		log.info(response.getContentAsString());
+	}
+}
