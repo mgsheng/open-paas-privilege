848466a4a28d203f73c5fe0f8e00648b7c29cb9f
guxuyang <it_gxy@163.com>
2017-09-01 14:42:16 +0800

    单元测试

--- privilege-service/src/test/java/cn/com/open/opensass/privilege/base/BaseTest.java
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/base/BaseTest.java
@@ -1,17 +1,23 @@
 package cn.com.open.opensass.privilege.base;
 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration({ "classpath*:/spring/*.xml", "file:src/main/webapp/WEB-INF/soc-servlet.xml"})
+@Transactional
 public class BaseTest  {
 
     protected Logger log = LoggerFactory.getLogger(this.getClass());
 
-
+    @Test
+    public void demo() {
+       log.info("hehe");
+    }
 
 }
