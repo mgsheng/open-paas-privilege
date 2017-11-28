c6ab9c2ecf38d69c2df179aabf2d89d15944e0f6
Li Li <yongxs@163.com>
2017-11-28 15:19:19 +0800

    【权限服务】验证用户单元测试

--- privilege-service/src/test/java/cn/com/open/opensass/privilege/VerifyUserPrivilegeControllerTest.java
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/VerifyUserPrivilegeControllerTest.java
@@ -19,9 +19,10 @@ import java.io.UnsupportedEncodingException;
 public class VerifyUserPrivilegeControllerTest extends BaseTest {
 
 	@Autowired
-	private VerifyUserPrivilegeController fixture;
+	private VerifyUserPrivilegeController verifyUserPrivilegeController;
 	@Autowired
 	private RedisClientTemplate redisClient;
+	public static final String SIGN = RedisConstant.SIGN;
 
 	/**
 	 * 正常操作.
@@ -39,9 +40,29 @@ public class VerifyUserPrivilegeControllerTest extends BaseTest {
 		request.addParameter("appUserId", appUserid);
 		request.addParameter("optUrl", optUrl);
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.verifyUserPrivilege(request, response);
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
 		log.info(response.getContentAsString());
 		Assert.assertEquals("1", JSONObject.parseObject(response.getContentAsString()).getString("status"));
+	}/**
+	 * 判断是否是管理员.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void verifyAdminUserPrivilege() throws UnsupportedEncodingException {
+		String appsecret = "67d73cec5d6b4c8a8a9883748f4066fe";
+		String appId = "8";
+		String appKey = "df6bda1a157d49cea82447c3e925dd6d";
+		String appUserid = "1000478";
+		String optUrl = "/Query/condition/lcenter_8.aspx?Stat=_MatricTestStu";
+
+		MockHttpServletRequest request = Signature.getSignatureRequest(appsecret, appId, appKey);
+		request.addParameter("appUserId", appUserid);
+		request.addParameter("optUrl", optUrl);
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
+		System.out.println("verifyAdminUserPrivilege:"+response.getContentAsString());
+		log.info(response.getContentAsString());
+		Assert.assertEquals("0", JSONObject.parseObject(response.getContentAsString()).getString("status"));
 	}
 	/**
 	 * 参数为空.
@@ -53,7 +74,7 @@ public class VerifyUserPrivilegeControllerTest extends BaseTest {
 		request.addParameter("appUserId","3094776");
 		request.addParameter("optUrl", "/Query/condition/lcenter_8.aspx?Stat=_MatricTestStu");
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.verifyUserPrivilege(request, response);
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
 		log.info(response.getContentAsString());
 		System.out.println("verifyUserPrivilegeParamNull:"+response.getContentAsString());
 		Assert.assertEquals("1", JSONObject.parseObject(response.getContentAsString()).getString("status"));
@@ -69,7 +90,7 @@ public class VerifyUserPrivilegeControllerTest extends BaseTest {
 		request.addParameter("optUrl", "");
 		redisClient.del(RedisConstant.APP_INFO +  CommonEnum.APP_ID.getDisplay());
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.verifyUserPrivilege(request, response);
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
 		log.info(response.getContentAsString());
 		System.out.println("verifyUserPrivilegeAppNull:"+response.getContentAsString());
 		Assert.assertEquals("0", JSONObject.parseObject(response.getContentAsString()).getString("status"));
@@ -87,7 +108,7 @@ public class VerifyUserPrivilegeControllerTest extends BaseTest {
 		request.addParameter("appKey","test");
 		redisClient.del(RedisConstant.APP_INFO +  CommonEnum.APP_ID.getDisplay());
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.verifyUserPrivilege(request, response);
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
 		log.info(response.getContentAsString());
 		System.out.println("verifyUserPrivilegeValidFailed:"+response.getContentAsString());
 		Assert.assertEquals("0", JSONObject.parseObject(response.getContentAsString()).getString("status"));
@@ -103,9 +124,37 @@ public class VerifyUserPrivilegeControllerTest extends BaseTest {
 		request.addParameter("optUrl", "/Query/condition/lcenter_8.aspx?Stat=_MatricTestStu");
 		redisClient.del(RedisConstant.APP_INFO +  CommonEnum.APP_ID.getDisplay());
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.verifyUserPrivilege(request, response);
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
 		log.info(response.getContentAsString());
 		System.out.println("verifyUserPrivilegeNoUserId:"+response.getContentAsString());
 		Assert.assertEquals("0", JSONObject.parseObject(response.getContentAsString()).getString("status"));
+	}
+	/**
+	 * 若没有该用户 返回认证失败.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void verifyUserPrivilegeDelRedis() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+		request.addParameter("appUserId","3094776");
+		request.addParameter("optUrl", "/Query/condition/lcenter_8.aspx?Stat=_MatricTestStu");
+		redisClient.del(RedisConstant.APP_INFO +  CommonEnum.APP_ID.getDisplay());
+		StringBuilder redisUserPrivilegeKey=new StringBuilder(RedisConstant.PUBLICSERVICE_CACHE);
+		redisUserPrivilegeKey.append(RedisConstant.USER_CACHE_INFO);
+		redisUserPrivilegeKey.append(CommonEnum.APP_ID.getDisplay());
+		redisUserPrivilegeKey.append(SIGN);
+		redisUserPrivilegeKey.append("3094776");
+		redisClient.setObject(String.valueOf(redisUserPrivilegeKey),"test");
+		StringBuilder redisUserAllPrivilegeKey=new StringBuilder(RedisConstant.PUBLICSERVICE_CACHE);
+		redisUserAllPrivilegeKey.append(RedisConstant.USER_ALL_CACHE_INFO);
+		redisUserAllPrivilegeKey.append(CommonEnum.APP_ID.getDisplay());
+		redisUserAllPrivilegeKey.append(SIGN);
+		redisUserAllPrivilegeKey.append("3094776");
+		redisClient.setObject(String.valueOf(redisUserAllPrivilegeKey),"test");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		verifyUserPrivilegeController.verifyUserPrivilege(request, response);
+		log.info(response.getContentAsString());
+		System.out.println("verifyUserPrivilegeNoUserId:"+response.getContentAsString());
+		Assert.assertEquals("1", JSONObject.parseObject(response.getContentAsString()).getString("status"));
 	}
 }
