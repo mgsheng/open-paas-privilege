00fea0527a84c05f138c902f7c21246653cd2273
Li Li <yongxs@163.com>
2017-11-28 17:30:19 +0800

    【权限服务】单元测试新增覆盖率

--- privilege-service/src/test/java/cn/com/open/opensass/privilege/FunctionGetPrivilegeControllerTest.java
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/FunctionGetPrivilegeControllerTest.java
@@ -2,6 +2,9 @@ package cn.com.open.opensass.privilege;
 
 import cn.com.open.opensass.privilege.api.FunctionGetPrivilegeController;
 import cn.com.open.opensass.privilege.base.BaseTest;
+import cn.com.open.opensass.privilege.redis.impl.RedisClientTemplate;
+import cn.com.open.opensass.privilege.redis.impl.RedisConstant;
+import cn.com.open.opensass.privilege.signature.CommonEnum;
 import cn.com.open.opensass.privilege.signature.Signature;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
@@ -17,6 +20,8 @@ public class FunctionGetPrivilegeControllerTest extends BaseTest {
 
     @Autowired
     private FunctionGetPrivilegeController functionGetPrivilegeController;
+    @Autowired
+    private RedisClientTemplate redisClientTemplate;
 
     @Test //  /function/getFunctionIdList")
     public void modifyFunction() throws UnsupportedEncodingException {
@@ -30,5 +35,74 @@ public class FunctionGetPrivilegeControllerTest extends BaseTest {
         functionGetPrivilegeController.modifyFunction(request, response);
         JSONObject json = JSON.parseObject(response.getContentAsString());
         Assert.assertEquals("0", json.getString("status"));
+    }
+    /**
+     * 获取数据为空.
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void modifyFunctionParamNull() throws UnsupportedEncodingException {
+        MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+        request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+        request.addParameter("appUserId", "");
+        request.addParameter("resourceId", "151,185");
+        redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        functionGetPrivilegeController.modifyFunction(request, response);
+        log.info(response.getContentAsString());
+        System.out.println("modifyFunctionParamNull:"+response.getContentAsString());
+        Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
+    }
+    /**
+     * app为空,暂无数据.
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void modifyFunctionAppNull() throws UnsupportedEncodingException {
+        MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+        request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+        request.addParameter("appUserId", "3094776");
+        request.addParameter("resourceId", "151,185");
+        redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        functionGetPrivilegeController.modifyFunction(request, response);
+        log.info(response.getContentAsString());
+        System.out.println("modifyFunctionAppNull:"+response.getContentAsString());
+        Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
+    }
+    /**
+     * 正常操作，有数据.
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void modifyFunctionSuccess() throws UnsupportedEncodingException {
+        MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+        request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+        request.addParameter("appUserId", "1503143");
+        request.addParameter("resourceId", "1326,10629");
+        redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        functionGetPrivilegeController.modifyFunction(request, response);
+        log.info(response.getContentAsString());
+        System.out.println("modifyFunctionSuccess:"+response.getContentAsString());
+        Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("1"));
+    }
+    /**
+     * 验证失败.
+     * @throws UnsupportedEncodingException
+     */
+    @Test
+    public void modifyFunctionValidFailed() throws UnsupportedEncodingException {
+        MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+        request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+        request.addParameter("appUserId", "1503143");
+        request.addParameter("resourceId", "1326,10629");
+        request.removeParameter("appKey");
+        request.addParameter("appKey","test");
+        MockHttpServletResponse response = new MockHttpServletResponse();
+        functionGetPrivilegeController.modifyFunction(request, response);
+        log.info(response.getContentAsString());
+        System.out.println("modifyFunctionValidFailed:"+response.getContentAsString());
+        Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
     }
 }
--- privilege-service/src/test/java/cn/com/open/opensass/privilege/UserRoleRedisPrivilegeControllerTest.java
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/UserRoleRedisPrivilegeControllerTest.java
@@ -2,6 +2,10 @@ package cn.com.open.opensass.privilege;
 
 import cn.com.open.opensass.privilege.api.UserRoleRedisPrivilegeController;
 import cn.com.open.opensass.privilege.base.BaseTest;
+import cn.com.open.opensass.privilege.redis.impl.RedisClientTemplate;
+import cn.com.open.opensass.privilege.redis.impl.RedisConstant;
+import cn.com.open.opensass.privilege.signature.CommonEnum;
+import cn.com.open.opensass.privilege.signature.Signature;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -14,7 +18,10 @@ import java.io.UnsupportedEncodingException;
 public class UserRoleRedisPrivilegeControllerTest extends BaseTest {
 
 	@Autowired
-	private UserRoleRedisPrivilegeController fixture;
+	private UserRoleRedisPrivilegeController userRoleRedisPrivilegeController;
+	@Autowired
+	private RedisClientTemplate redisClientTemplate;
+	public static final String SIGN = RedisConstant.SIGN;
 
 	@Test 	// 	/userRole/getUserRoleRedis")
 	public void getUserPrivilege() throws UnsupportedEncodingException {
@@ -22,7 +29,7 @@ public class UserRoleRedisPrivilegeControllerTest extends BaseTest {
 		request.addParameter("appId", "8");
 		request.addParameter("appUserId", "3094776");
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.getUserPrivilege(request, response);
+		userRoleRedisPrivilegeController.getUserPrivilege(request, response);
 		log.info(response.getContentAsString());
 		Assert.assertTrue(response.getContentAsString().contains("functionList"));
 	}
@@ -33,7 +40,7 @@ public class UserRoleRedisPrivilegeControllerTest extends BaseTest {
 		request.addParameter("appId", "8");
 		request.addParameter("appUserId", "3094776");
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.updateUserRoleRedis(request, response);
+		userRoleRedisPrivilegeController.updateUserRoleRedis(request, response);
 		log.info(response.getContentAsString());
 		Assert.assertTrue(response.getContentAsString().contains("functionList"));
 	}
@@ -44,10 +51,87 @@ public class UserRoleRedisPrivilegeControllerTest extends BaseTest {
 		request.addParameter("appId", "8");
 		request.addParameter("appUserId", "3094776");
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.delUserRoleRedis(request, response);
+		userRoleRedisPrivilegeController.delUserRoleRedis(request, response);
 		log.info(response.getContentAsString());
 		Assert.assertEquals("Success", response.getContentAsString());
 	}
 
 
+	/**
+	 * 参数为空.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void getUserPrivilegeParamNull() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+		request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+		request.addParameter("appUserId", "");
+		redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		userRoleRedisPrivilegeController.getUserPrivilege(request, response);
+		log.info(response.getContentAsString());
+		System.out.println("getUserPrivilegeParamNull:"+response.getContentAsString());
+		Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
+	}
+	/**
+	 * 数据为空.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void getUserPrivilegeUserNoExist() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+		request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+		request.addParameter("appUserId", "test_test1");
+		redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		userRoleRedisPrivilegeController.getUserPrivilege(request, response);
+		log.info(response.getContentAsString());
+		System.out.println("getUserPrivilegeUserNoExist:"+response.getContentAsString());
+		Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
+	}
+
+	/**
+	 * 参数为空.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void updateUserRoleRedisParamNull() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+		request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+		request.addParameter("appUserId", "");
+		redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		userRoleRedisPrivilegeController.updateUserRoleRedis(request, response);
+		log.info(response.getContentAsString());
+		System.out.println("updateUserRoleRedisParamNull:"+response.getContentAsString());
+		Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
+	}
+	/**
+	 * 删除缓存.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void updateUserRoleRedisDelRedis() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = Signature.getSignatureRequest(CommonEnum.APP_SECRET.getDisplay(), CommonEnum.APP_ID.getDisplay(), CommonEnum.APP_KEY.getDisplay());
+		request.addParameter("appId",CommonEnum.APP_ID.getDisplay());
+		request.addParameter("appUserId", "test_test");
+		redisClientTemplate.del(RedisConstant.APP_INFO+CommonEnum.APP_ID.getDisplay());
+		StringBuilder redisUserPrivilegeKey=new StringBuilder(RedisConstant.PUBLICSERVICE_CACHE);
+		redisUserPrivilegeKey.append(RedisConstant.USER_CACHE_INFO);
+		redisUserPrivilegeKey.append(CommonEnum.APP_ID.getDisplay());
+		redisUserPrivilegeKey.append(SIGN);
+		redisUserPrivilegeKey.append("3094776");
+		redisClientTemplate.setObject(String.valueOf(redisUserPrivilegeKey),"test");
+		StringBuilder redisUserAllPrivilegeKey=new StringBuilder(RedisConstant.PUBLICSERVICE_CACHE);
+		redisUserAllPrivilegeKey.append(RedisConstant.USER_ALL_CACHE_INFO);
+		redisUserAllPrivilegeKey.append(CommonEnum.APP_ID.getDisplay());
+		redisUserAllPrivilegeKey.append(SIGN);
+		redisUserAllPrivilegeKey.append("3094776");
+		redisClientTemplate.setObject(String.valueOf(redisUserAllPrivilegeKey),"test");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		userRoleRedisPrivilegeController.updateUserRoleRedis(request, response);
+		log.info(response.getContentAsString());
+		System.out.println("updateUserRoleRedisDelRedis:"+response.getContentAsString());
+		Assert.assertTrue(net.sf.json.JSONObject.fromObject(response.getContentAsString()).get("status").equals("0"));
+	}
 }
