34b964a84ab0e057eeea118edd15634c24d67215
Li Li <yongxs@163.com>
2017-11-28 15:56:46 +0800

    【权限服务】权限资源查询接口格式化代码

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/api/ResourceGetPrivilegeController.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/api/ResourceGetPrivilegeController.java
@@ -27,82 +27,82 @@ import cn.com.open.opensass.privilege.tools.OauthSignatureValidateHandler;
 import cn.com.open.opensass.privilege.tools.WebUtils;
 
 /**
- *  权限资源查询接口
+ * 权限资源查询接口
  */
 @Controller
 @RequestMapping("/resource/")
-public class ResourceGetPrivilegeController extends BaseControllerUtil{
-	private static final Logger log = LoggerFactory.getLogger(ResourceGetPrivilegeController.class);
-	@Autowired
-	private PrivilegeResourceService privilegeResourceService;
-	@Autowired
-	private PrivilegeFunctionService privilegeFunctionService;
-	@Autowired
-	private AppService appService;
-	@Autowired
-	private RedisClientTemplate redisClient;
+public class ResourceGetPrivilegeController extends BaseControllerUtil {
+    private static final Logger log = LoggerFactory.getLogger(ResourceGetPrivilegeController.class);
+    @Autowired
+    private PrivilegeResourceService privilegeResourceService;
+    @Autowired
+    private PrivilegeFunctionService privilegeFunctionService;
+    @Autowired
+    private AppService appService;
+    @Autowired
+    private RedisClientTemplate redisClient;
+
     @RequestMapping("getResPrivilege")
-    public void getResPrivilege(HttpServletRequest request,HttpServletResponse response) {
-    	String menuId=request.getParameter("menuId");
-    	String appId=request.getParameter("appId");
-    	String start=request.getParameter("start");
-    	String limit=request.getParameter("Limit");
-    	String resourceLevel=request.getParameter("resourceLevel");
-    	Map<String, Object> map=new HashMap<String, Object>();
-    	log.info("====================query start======================");
-    	if(!paraMandatoryCheck(Arrays.asList(start,appId,limit))){
-    		  paraMandaChkAndReturn(10000, response,"必传参数中有空值");
-              return;	
-    	}
-    	App app = (App) redisClient.getObject(RedisConstant.APP_INFO+appId);
-	    if(app==null)
-		   {
-			   app=appService.findById(Integer.parseInt(appId));
-			   redisClient.setObject(RedisConstant.APP_INFO+appId, app);
-		  }
-    	Boolean f=OauthSignatureValidateHandler.validateSignature(request,app);
-		if(!f){
-			WebUtils.paraMandaChkAndReturn(5, response,"认证失败");
-			return;
-		}
-    	List<PrivilegeResource>lists=privilegeResourceService.findResourcePage(menuId, appId,Integer.parseInt(start),Integer.parseInt(limit), resourceLevel);
-    	if(lists!=null&&lists.size()>0){
-    		Map<String, Object> resourceMap=null;
-    		map.put("status", "1");
-    		map.put("count", lists.size());
-    		List<Map<String, Object>> list=new ArrayList<Map<String, Object>>();
-    		for(int i=0;i<lists.size();i++){
-    			resourceMap=new HashMap<String,Object>();
-    			resourceMap.put("appId", lists.get(i).getAppId());
-    			resourceMap.put("resourceId", lists.get(i).getResourceId());
-    			resourceMap.put("resourceLevel", lists.get(i).getResourceLevel());
-    			resourceMap.put("resourceName", lists.get(i).getResourceName());
-    			resourceMap.put("resourceRule", lists.get(i).getResourceRule());
-    			resourceMap.put("dislayOrder", lists.get(i).getDisplayOrder());
-    			resourceMap.put("menuId", lists.get(i).getMenuId());
-    			resourceMap.put("baseUrl", lists.get(i).getBaseUrl());
-    			resourceMap.put("status", lists.get(i).getStatus());
-    			if(!nullEmptyBlankJudge(lists.get(i).getResourceId())){
-    				List<Map<String, Object>> functionList=privilegeFunctionService.getFunctionMap(lists.get(i).getResourceId(),lists.get(i).getAppId());
-        			if(functionList!=null&&functionList.size()>0){
-        			 resourceMap.put("functionList", functionList);	
-        			}
-    			}
-    			list.add(resourceMap);
-    		}
-    		//JSON json =JSONObject.fromObject(resourceMap);
-    		map.put("resourceList",list);
-    	}else{
-    		map.put("status", "0");
-    		map.put("error_code", "10001");
-    	}
-    	if(map.get("status")=="0"){
-    		writeErrorJson(response,map);
-    	}else{
-    		writeSuccessJson(response,map);
-    	}
-    	//OauthControllerLog.log(startTime, guid, source_id, app, map,userserviceDev);
+    public void getResPrivilege(HttpServletRequest request, HttpServletResponse response) {
+        String menuId = request.getParameter("menuId");
+        String appId = request.getParameter("appId");
+        String start = request.getParameter("start");
+        String limit = request.getParameter("Limit");
+        String resourceLevel = request.getParameter("resourceLevel");
+        Map<String, Object> map = new HashMap<String, Object>();
+        log.info("====================query start======================");
+        if (!paraMandatoryCheck(Arrays.asList(start, appId, limit))) {
+            paraMandaChkAndReturn(10000, response, "必传参数中有空值");
+            return;
+        }
+        App app = (App) redisClient.getObject(RedisConstant.APP_INFO + appId);
+        if (app == null) {
+            app = appService.findById(Integer.parseInt(appId));
+            redisClient.setObject(RedisConstant.APP_INFO + appId, app);
+        }
+        Boolean f = OauthSignatureValidateHandler.validateSignature(request, app);
+        if (!f) {
+            WebUtils.paraMandaChkAndReturn(5, response, "认证失败");
+            return;
+        }
+        List<PrivilegeResource> lists = privilegeResourceService.findResourcePage(menuId, appId, Integer.parseInt(start), Integer.parseInt(limit), resourceLevel);
+        if (lists != null && lists.size() > 0) {
+            Map<String, Object> resourceMap = null;
+            map.put("status", "1");
+            map.put("count", lists.size());
+            List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+            for (int i = 0; i < lists.size(); i++) {
+                resourceMap = new HashMap<String, Object>();
+                resourceMap.put("appId", lists.get(i).getAppId());
+                resourceMap.put("resourceId", lists.get(i).getResourceId());
+                resourceMap.put("resourceLevel", lists.get(i).getResourceLevel());
+                resourceMap.put("resourceName", lists.get(i).getResourceName());
+                resourceMap.put("resourceRule", lists.get(i).getResourceRule());
+                resourceMap.put("dislayOrder", lists.get(i).getDisplayOrder());
+                resourceMap.put("menuId", lists.get(i).getMenuId());
+                resourceMap.put("baseUrl", lists.get(i).getBaseUrl());
+                resourceMap.put("status", lists.get(i).getStatus());
+                if (!nullEmptyBlankJudge(lists.get(i).getResourceId())) {
+                    List<Map<String, Object>> functionList = privilegeFunctionService.getFunctionMap(lists.get(i).getResourceId(), lists.get(i).getAppId());
+                    if (functionList != null && functionList.size() > 0) {
+                        resourceMap.put("functionList", functionList);
+                    }
+                }
+                list.add(resourceMap);
+            }
+            //JSON json =JSONObject.fromObject(resourceMap);
+            map.put("resourceList", list);
+        } else {
+            map.put("status", "0");
+            map.put("error_code", "10001");
+        }
+        if (map.get("status") == "0") {
+            writeErrorJson(response, map);
+        } else {
+            writeSuccessJson(response, map);
+        }
+        //OauthControllerLog.log(startTime, guid, source_id, app, map,userserviceDev);
         return;
-    	
+
     }
 }
