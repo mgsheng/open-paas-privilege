6d284abfd7efe39f3c51c642e77a041776981ee0
Li Li <yongxs@163.com>
2017-11-28 15:38:28 +0800

    【权限服务】getRole单元测试

--- privilege-service/src/test/java/cn/com/open/opensass/privilege/PrivilegeRoleControllerTest.java
+++ privilege-service/src/test/java/cn/com/open/opensass/privilege/PrivilegeRoleControllerTest.java
@@ -2,6 +2,7 @@ package cn.com.open.opensass.privilege;
 
 import cn.com.open.opensass.privilege.api.PrivilegeRoleController;
 import cn.com.open.opensass.privilege.base.BaseTest;
+import com.alibaba.fastjson.JSONObject;
 import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,17 +14,55 @@ import java.io.UnsupportedEncodingException;
 public class PrivilegeRoleControllerTest extends BaseTest {
 
 	@Autowired
-	private PrivilegeRoleController fixture;
+	private PrivilegeRoleController privilegeRoleController;
 
-	@Test // /role/getRole
+	/**
+	 * 正常获取数据.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
 	public void getRole() throws UnsupportedEncodingException {
 		MockHttpServletRequest request = new MockHttpServletRequest();
 		request.addParameter("appId", "8");
 		request.addParameter("start", "0");
 		request.addParameter("limit", "10");
 		MockHttpServletResponse response = new MockHttpServletResponse();
-		fixture.getRole(request, response);
+		privilegeRoleController.getRole(request, response);
+		System.out.println("getRole:"+response.getContentAsString());
 		log.info(response.getContentAsString());
 		Assert.assertTrue(response.getContentAsString().contains("roleList"));
+	}
+	/**
+	 * 正常获取数据.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void getRoleType() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.addParameter("appId", "8");
+		request.addParameter("start", "0");
+		request.addParameter("limit", "10");
+		request.addParameter("roleType", "2");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		privilegeRoleController.getRole(request, response);
+		System.out.println("getRole:"+response.getContentAsString());
+		log.info(response.getContentAsString());
+		Assert.assertTrue(response.getContentAsString().contains("roleList"));
+	}
+	/**
+	 * 参数为空.
+	 * @throws UnsupportedEncodingException
+	 */
+	@Test
+	public void getRoleParam() throws UnsupportedEncodingException {
+		MockHttpServletRequest request = new MockHttpServletRequest();
+		request.addParameter("appId", "");
+		request.addParameter("start", "0");
+		request.addParameter("limit", "10");
+		MockHttpServletResponse response = new MockHttpServletResponse();
+		privilegeRoleController.getRole(request, response);
+		System.out.println("getRoleParam:"+response.getContentAsString());
+		log.info(response.getContentAsString());
+		Assert.assertEquals("0", JSONObject.parseObject(response.getContentAsString()).getString("status"));
 	}
 }
