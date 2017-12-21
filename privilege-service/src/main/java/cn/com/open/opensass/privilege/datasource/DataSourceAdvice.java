bbd6249860baad3b926deb7572a6b365a0fb5a64
Li Li <yongxs@163.com>
2017-12-21 15:45:14 +0800

    禁止输出日志，除了异常错误之外

--- privilege-service/src/main/java/cn/com/open/opensass/privilege/datasource/DataSourceAdvice.java
+++ privilege-service/src/main/java/cn/com/open/opensass/privilege/datasource/DataSourceAdvice.java
@@ -9,7 +9,7 @@ import org.springframework.aop.ThrowsAdvice;
 public class DataSourceAdvice implements MethodBeforeAdvice, AfterReturningAdvice, ThrowsAdvice {  
     // service方法执行之前被调用  
     public void before(Method method, Object[] args, Object target) throws Throwable {  
-        System.out.println("切入点: " + target.getClass().getName() + "类中" + method.getName() + "方法");  
+//        System.out.println("切入点: " + target.getClass().getName() + "类中" + method.getName() + "方法");
         if(method.getName().startsWith("add")   
             || method.getName().startsWith("create")  
             || method.getName().startsWith("archive")  
@@ -21,11 +21,11 @@ public class DataSourceAdvice implements MethodBeforeAdvice, AfterReturningAdvic
               || method.getName().contains("Token")
               || method.getName().contains("Authentication")
             || method.getName().startsWith("remove")){  
-            System.out.println("切换到: master");  
+//            System.out.println("切换到: master");
             DataSourceSwitcher.setMaster();  
         }  
         else  {  
-            System.out.println("切换到: slave");  
+//            System.out.println("切换到: slave");
             DataSourceSwitcher.setSlave();  
         }  
     }  
@@ -37,7 +37,7 @@ public class DataSourceAdvice implements MethodBeforeAdvice, AfterReturningAdvic
     // 抛出Exception之后被调用  
     public void afterThrowing(Method method, Object[] args, Object target, Exception ex) throws Throwable {  
         DataSourceSwitcher.setSlave();  
-        System.out.println("出现异常,切换到: slave");  
+//        System.out.println("出现异常,切换到: slave");
     }  
   
 }  
